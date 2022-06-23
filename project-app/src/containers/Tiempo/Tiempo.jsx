d64dc5c10cb0b20f9e83c84bb6d2a52e75cbb486
obordonado <obordonado@protonmail.com>
2022-06-23 21:12:03 +0200

    external example

--- project-app/src/containers/Tiempo/Tiempo.jsx
+++ project-app/src/containers/Tiempo/Tiempo.jsx
@@ -1,6 +1,6 @@
 import { useState, useEffect } from "react";
 import axios from 'axios'
-import WeatherCard from '../../components/WeatherCard/WeatherCard'
+import WeatherCard from "../../components/Weather/WeatherCard";
 
 import './Tiempo.css'
 
